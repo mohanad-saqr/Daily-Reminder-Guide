41ca8cc2aa143961d3f9faf53295144352405f44
mohanad.saqr <mohanad.saqr@alum.utoronto.ca>
2024-01-28 12:54:49 -0500

    css and js

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('node:path'); 
+
+app.use(express.json());
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 const { engine } = require('express-handlebars');
 
@@ -12,8 +22,21 @@ app.set('view engine', 'handlebars');
 
 app.get('/', (request, response) => {
   response.render('index');
-})
-  
+});
+
+app.post('/authenticate', (req, res) => {
+
+  const userName = req.body.useName
+ console.log(req.body.useName);
+ const pwd = req.body.pwd
+ console.log(req.body.pwd);
+ console.log(req.body);
+
+ 
+
+    res.render("home")
+  });  
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
